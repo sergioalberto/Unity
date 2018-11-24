2155a33f0828e3cb17b2fc47c3544b51f5a0ac0b
Sergio Quiros <squiros@growthaccelerationpartners.com>
2018-11-24 15:14:16 -0600

    Ninja Adventure

---
+++ Ninja Adventure/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D myRigidbody;
+
+	[SerializeField]
+	private float movementSpeed;
+	private bool facingRight;
+
+	// Use this for initialization
+	void Start () {
+		facingRight = true;
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizoltal = Input.GetAxis("Horizontal");
+		HandleMovement(horizoltal);
+		Flip(horizoltal);
+	}
+
+	private void HandleMovement(float horizoltal){
+		myRigidbody.velocity = new Vector2(horizoltal * movementSpeed, myRigidbody.velocity.y);
+	}
+
+	private void Flip(float horizoltal){
+		if (horizoltal > 0 && !facingRight || horizoltal < 0 && facingRight){
+			facingRight = !facingRight;
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
+
+}
