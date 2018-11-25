a974634c1c946ed72f1eaf23cbf85e31061d5d0b
Sergio GQ <sergioalbertogq@gmail.com>
2018-11-25 08:48:26 -0600

    9 - Jumping

--- Ninja Adventure/Assets/Scripts/Player.cs
+++ Ninja Adventure/Assets/Scripts/Player.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	public float movementSpeed;
+	public float jumpForce; // To indicate how high we want to jump
+
 	private Rigidbody2D _myRigidbody;
 	private Animator _myAnimator;
 	[SerializeField]
 	private bool _facingRight;
 	private bool _attack;
 	private bool _slide;
-
-	public float movementSpeed;
+	[SerializeField]
+	private Transform[] _groundPoints;
+	[SerializeField]
+	private float _groundRadius;
+	[SerializeField]
+	private LayerMask _whatIsGround;
+	private bool _isGrounded;
+	private bool _jump;
+	[SerializeField]
+	private bool _airControl;
 
 	// Use this for initialization
 	void Start () {
@@ -28,6 +39,7 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		float horizoltal = Input.GetAxis("Horizontal");
+		_isGrounded = IsGrounded();
 		HandleMovement(horizoltal);
 		Flip(horizoltal);
 		HandleAttack();
@@ -35,8 +47,13 @@ public class Player : MonoBehaviour {
 	}
 
 	private void HandleMovement(float horizoltal){
-		if (!_myAnimator.GetBool("slide") && !this._myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")){ // Do not move it while it is attacking
+		if (!_myAnimator.GetBool("slide") && !this._myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack") 
+			&& (_isGrounded || _airControl)){ // Do not move it while it is attacking
 			_myRigidbody.velocity = new Vector2(horizoltal * movementSpeed, _myRigidbody.velocity.y);
+		}
+		if (_isGrounded && _jump){
+			_isGrounded = false;
+			_myRigidbody.AddForce(new Vector2(0, jumpForce));
 		}
 		if (_slide && !this._myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Slide")){
 			_myAnimator.SetBool("slide", true);
@@ -58,6 +75,8 @@ public class Player : MonoBehaviour {
 			_attack = true;
 		} else if (Input.GetKeyDown(KeyCode.LeftControl)){
 			_slide = true;
+		} else if (Input.GetKeyDown(KeyCode.Space)){
+			_jump = true;
 		}
 	}
 
@@ -73,6 +92,20 @@ public class Player : MonoBehaviour {
 	private void ResetValues(){
 		_attack = false;
 		_slide = false;
+		_jump = false;
 	}
 
+	private bool IsGrounded(){
+		if (_myRigidbody.velocity.y <= 0){ // if we are standing on the ground
+			foreach (Transform point in _groundPoints){
+				Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, _groundRadius, _whatIsGround); // ckeck if these points are colliding with something
+				for (int count = 0; count < colliders.Length; count ++){
+					if (colliders[count].gameObject != gameObject){ // if the object is different from the player (colliding something else than the player without feet)
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
